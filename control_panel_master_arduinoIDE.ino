30196ed2c625698c517a57c5b755710fad895243
Shawn Lin <spig100.roc@gmail.com>
2026-01-12 23:38:07 +0800

    feat: Add string-sending function

--- control_panel_master_arduinoIDE.ino
+++ control_panel_master_arduinoIDE.ino
@@ -1,12 +1,61 @@
 #include <WiFi.h>
-#include <esp_wifi.h>
+#include <esp_now.h>
+
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Replace it with your master ESP32's MAC Address.
+
+typedef struct struct_message{
+  char a[32];
+} message;
+
+message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// Call message for checking if the data is sent succesfully or not.
+void OnDataSent(const wifi_tx_info_t *tx_info, esp_now_send_status_t status) {
+  Serial.print("Last Packet Send Status: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Succeed" : "Failed");
+
+}
 
 void setup() {
   // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK){
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  // Pairing with slaves.
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Peer is not add successfully.");
+    return;
+  }
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  strcpy(myData.a, "Test message.");
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  if (result == ESP_OK) {
+    Serial.println("Message sent successfully.");
+  } 
+  else{
+    Serial.println("Message sent failed");
+  }
+  delay(2000);
 }
